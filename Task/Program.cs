47768b30643e6ed7fb12668064d2ece8fabacb17
Artem_Sidorenko <artem70947@gmail.com>
2023-06-09 16:24:16 +0300

    часть 1: исходный массив строк

---
+++ Task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Получение исходного массива строк
+
+Console.Write("Введите количество строк в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
